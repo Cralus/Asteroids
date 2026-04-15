70803e48180e616e681b44a11c547e54f2deba0b
Cralus <defactopersona@gmail.com>
2026-04-15 11:29:59 +0100

    Added groups to main.py to clean up the code

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,8 +25,9 @@ def main():
                 return
         dt = clock.tick(60) / 1000
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
 
